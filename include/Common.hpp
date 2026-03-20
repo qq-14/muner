8ca6770a22729216ac64fc9715082e4f567c1b92
你的GitHub用户名 <GitHub>
2026-03-20 10:53:51 +0800

    update

--- include/Common.hpp
+++ include/Common.hpp
@@ -1,1 +1,11 @@
-#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+struct miner {
+    float L;
+    float W;
+    float Center;
+    float LeftCenter;
+    float RightCenter;
+}
