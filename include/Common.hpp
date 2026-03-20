a48f240ecf9a721c67085a02f5900cc20f19b1d7
你的GitHub用户名 <GitHub>
2026-03-20 10:56:41 +0800

    update

--- include/Common.hpp
+++ include/Common.hpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
+
 using namespace std;
 using namespace cv;
+
 struct miner {
-    float L;
-    float W;
-    float Center;
-    float LeftCenter;
-    float RightCenter;
-}
+    float L; // 长
+    float W; // 宽
+    float Center; //中心点
+    vector<Point2f> len_points; //长边中心 
+};
