1b3f9733a81da1fb982b60865cfc4ace3673e4ea
qiu <Qiu-ux872>
2026-05-12 15:00:24 +0800

    update

---
+++ build_win/CMakeFiles/TrafficLightRecognition.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrafficLightRecognition.dir/src/PreProcess.cpp.obj"
+  "CMakeFiles/TrafficLightRecognition.dir/src/PreProcess.cpp.obj.d"
+  "CMakeFiles/TrafficLightRecognition.dir/src/main.cpp.obj"
+  "CMakeFiles/TrafficLightRecognition.dir/src/main.cpp.obj.d"
+  "TrafficLightRecognition.exe"
+  "TrafficLightRecognition.pdb"
+  "libTrafficLightRecognition.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrafficLightRecognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
